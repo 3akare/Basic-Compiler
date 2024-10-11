ca5a0447a57ccd87f2ce9aaec24a16abeee12de6
3akare <bakaredavid007@gmail.com>
2024-10-11 15:51:12 +0100

    feat: read from file

--- Compiler.java
+++ Compiler.java
@@ -1,6 +1,18 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Compiler{
     public static void main(String[] args){
-        String source = "IF+-123 foo*THEN/";
+
+        if(args.length != 1){
+            System.out.println("Error: Compiler needs source file as argument.");
+            System.exit(1);
+        }
+
+        String source = readFile(args[0]);
+        System.out.println(source);
         Lexer lexer = new Lexer(source);
 
         Token token = lexer.getToken();
@@ -10,4 +22,21 @@ public class Compiler{
             token = lexer.getToken();
         }
     };
+
+    private static String readFile(String fileName) {
+        File file = new File(fileName);
+        String fileText = "";
+        try{
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNext()){
+                fileText += scanner.nextLine();
+                fileText += '\n';
+            }
+            scanner.close();
+            return fileText;
+        }catch (Exception e){
+            System.out.println("Error: File not found");
+            return null;
+        }
+    }
 }
